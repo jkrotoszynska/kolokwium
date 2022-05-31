9e369eee41289212642c740af9db6a5f4a2cc0db
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-05-31 11:33:12 +0200

    rok pierwszy

---
+++ kolokwium/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolokwium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisać program wpisujący własne imię do pierwszego pola tablicy, nazwisko do drugiego (dowolną metodą!), i wyświetlający zawartość tej tablicy
+            string[] ja = { "Justyna", "Krotoszyńska" };
+
+            foreach (string element in ja)
+            {
+                Console.WriteLine(element);
+            }
+
+            // Wyświetlić (pętla for) wszystkie liczby z zakresu 0-1500 podzielne przez długość własnego imienia LUB nazwiska 
+            // (żeby nie było wątpliwości - u mnie, gdybyśmy zaczynali od 1, byłoby to 7, 12, 14, 21, 24, 28...)
+            string imie = "Justyna";
+            string naziwsko = "Krotoszyńska";
+            int dlugosc_imienia = imie.Length;
+            int dlugosc_nazwiska = naziwsko.Length;
+
+            for (int i = 0; i <= 1500; i++)
+            {
+                if (i % dlugosc_imienia == 0 || i % dlugosc_nazwiska == 0) Console.WriteLine(i);
+            }
+            
+
+            // Napisać program symulujący wynik pojedynczego rzutu kostką w oparciu o generator liczb losowych
+            Random kostka = new Random();
+            int rzut = kostka.Next(1, 7);
+            Console.WriteLine("Wyrzucono {0}", rzut);
+
+
+            // Napisać program pobierający 2 integery od użytkownika i przechwytujący wyjątek dzielenia przez zero przy wyświetlaniu wyniku dzielenia jednego przez drugi
+            try
+            {
+                Console.Write("Podaj pierwszy integer: ");
+                int pierwszy = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Podaj drugi integer: ");
+                int drugi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Wynik to: {0}", pierwszy / drugi);
+            } 
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nie można podzielić przez zero!!!!");
+            }
+            
+            // Napisać program, w którym za punkt wyjścia bierzemy liczbę, którą mają Państwo na teamsach w mailu w 
+            //zakładce "kontakt" swojego profilu. Wyświetlamy wspomnianą liczbę i każdą o połowę mniejszą, aż osiągniemy miesiąc swojego urodzenia (pętla while).
+            int numer = 998;
+            int miesiac = 11;
+
+            while (numer != miesiac)
+            {
+                numer = numer / 2;
+                if (numer < miesiac)
+                {
+                    Console.WriteLine("Nie da się osiągnąć {0}", miesiac);
+                    break;
+                }
+                Console.WriteLine(numer);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ kolokwium/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("kolokwium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kolokwium")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("c1589d8c-7364-4f12-84da-59ccf3f2f6ec")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
